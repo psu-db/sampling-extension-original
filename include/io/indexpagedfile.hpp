888f6e0dfa40a1f2dfaec08928adb7256eb6a0cf
Douglas Rumbaugh <doug@douglasrumbaugh.com>
2022-05-26 22:12:09 -0400

    IndexPagedFile: Initial implementation

---
+++ include/io/indexpagedfile.hpp
@@ -0,0 +1,168 @@
+/*
+ * indexpagedfile.hpp 
+ * Douglas Rumbaugh
+ *
+ * An interface for accessing a file as a linked list of pages, along with an
+ * associated iterator. Note that this class buffers some important metadata in
+ * memory, which is flushed to disk automatically when the file is closed via
+ * this object, or when its destructor is called. Because of this, it is
+ * important to access the associated file only through IndexPagedFile's interface.
+ * If a raw pointer to the underlying DirectFile is used to access the file
+ * after a IndexPagedFile has been created, any operations done through the
+ * IndexPagedFile's interface are undefined.
+ *
+ * The IndexPagedFile object contains a one page internal buffer used for metadata
+ * updates if the NO_BUFFER_MANAGER macro is defined. (TODO: Otherwise, it will
+ * attempt to use a buffer manager associated with the global file manager).
+ */
+
+#ifndef INDEXPAGEDFILE_H
+#define INDEXPAGEDFILE_H
+#include <string>
+#include <memory>
+#include <cassert>
+
+#include "util/types.hpp"
+#include "util/base.hpp"
+#include "util/iterator.hpp"
+#include "io/directfile.hpp"
+#include "io/page.hpp"
+
+#include "io/pagedfile.hpp"
+
+//#define NO_BUFFER_MANAGER
+
+namespace lsm {
+namespace io {
+
+// The header stored in Page 0 of the file, not file-level
+// header information stored on each page.
+struct IndexPagedFileHeaderData {
+    PagedFileHeaderData paged_header;
+};
+
+constexpr size_t IndexPagedFileHeaderSize = MAXALIGN(sizeof(IndexPagedFileHeaderData));
+static_assert(IndexPagedFileHeaderSize <= parm::PAGE_SIZE);
+
+class IndexPagedFile : public PagedFile {
+public:
+    // TODO: Move these two to the file manager
+    static std::unique_ptr<IndexPagedFile> create(std::string fname, bool new_file, FileId flid);
+    static std::unique_ptr<IndexPagedFile> create_temporary(FileId flid);
+
+    /*
+     * Initialize an empty DirectFile for use with paged accesses.
+     * Specifically, allocates a header page (page 0) containing file-level
+     * information used by the IndexPagedFile class. Should only be called on an
+     * empty file, and must be called on a file prior to passing it into a
+     * IndexPagedFile constructor.
+     */
+    static int initialize(DirectFile *dfile, FileId flid);
+
+    /*
+     * Create a new IndexPagedFile object from a DirectFile. Note that the
+     * Directfile in question must have been passed through
+     * IndexPagedFile::initialize at some point in its existence, otherwise all
+     * operations upon the resulting IndexPagedFile object are undefined. If
+     * is_temp_file is set to true, then the file will be considered temporary,
+     * and automatically deleted from the filesystem when it is closed, or when
+     * the IndexPagedFile object is destructed.
+     */
+    IndexPagedFile(std::unique_ptr<DirectFile> dfile, bool is_temp_file);
+
+    /*
+     * Add a new page to the file and return its associated PageId. Note that
+     * PageIds are not necessarily monotonic, as freed pages will be recycled.
+     *
+     * If the allocation fails due to an IO error, or for some other reason,
+     * returns INVALID_PID.
+     */
+    PageId allocate_page() override;
+
+    PageId allocate_page_bulk(PageNum new_page_count) override;
+
+    /*
+     * Delete a specified page and add it to the free list. The page can
+     * still be accessed directly by PageId/PageNum, however it will no
+     * longer appear in iterators over the page. Freed pages can be recycled
+     * by allocate. Returns 1 on success and 0 on failure.
+     */
+    int free_page(PageId pid) override;
+
+    /*
+     * Same as free_page(PageId), but accepts a PageNum rather than a PageId.
+     */
+    int free_page(PageNum pnum) override;
+
+    /*
+     * Returns true if the class supports deleting pages via free_page, and
+     * false if not.
+     */
+    bool supports_free() override;
+
+    /*
+     * Converts a given PageNum into a PageId that is associated with this
+     * file.
+     */
+    PageId pnum_to_pid(PageNum pnum) override;
+
+    /*
+     * Returns the number of allocated paged in the file.
+     */
+    PageNum get_page_count() override;
+
+    /*
+     * Returns the ID of the first page within the file. If there are no
+     * allocated pages, returns INVALID_PID.
+     */
+    PageId get_first_pid() override;
+
+    /*
+     * Returns the ID of the last page within the file. If there are no
+     * allocated pages, returns INVALID_PID. If there is only one page
+     * allocated, then get_first_pid() == get_last_pid().
+     */
+    PageId get_last_pid() override;
+
+    /*
+     * Returns a PagefileIterator opened to the specified page. If INVALID_PID
+     * is provided as an argument, then the iterator will be open to the first
+     * page. If the provided page does not exist, or if the file has no pages,
+     * then returns nullptr. If the specified page exists on the free list,
+     * then all operations on the returned iterator are undefined.
+     */
+    std::unique_ptr<iter::GenericIterator<Page *>> start_scan(PageId pid=INVALID_PID) override;
+    /*
+     * Same as start_scan(PageId), but accepts a PageNum as an argument instead.
+     */
+    virtual std::unique_ptr<iter::GenericIterator<Page *>> start_scan(PageNum pnum=INVALID_PNUM) override;
+
+    /*
+     * Close this file. Returns 1 on success and 0 on failure.
+     */
+    int close_file() override;
+
+
+    int reopen_file() override;
+
+    /*
+     *  
+     */
+    ~IndexPagedFile() override;
+
+private:
+    static const PageNum header_page_pnum = INVALID_PNUM;
+
+    void flush_metadata();
+    IndexPagedFileHeaderData header_data;
+
+    #ifdef NO_BUFFER_MANAGER
+    void flush_buffer(PageNum pnum);
+    std::unique_ptr<byte> buffer;
+    #endif
+};
+
+}
+}
+
+#endif
---
+++ src/io/indexpagedfile.cpp
@@ -0,0 +1,190 @@
+/*
+ *
+ */
+#include "io/indexpagedfile.hpp"
+
+namespace lsm {
+namespace io {
+
+std::unique_ptr<IndexPagedFile> IndexPagedFile::create(std::string fname, bool new_file, FileId flid)
+{
+    auto dfile = DirectFile::create(fname, new_file);
+    if (new_file) {
+        IndexPagedFile::initialize(dfile.get(), flid);
+    }
+
+    return std::make_unique<IndexPagedFile>(std::move(dfile), false);
+}
+
+
+std::unique_ptr<IndexPagedFile> IndexPagedFile::create_temporary(FileId flid)
+{
+    std::string fname; // TODO: name generation
+    
+    auto dfile = DirectFile::create(fname, true);
+    IndexPagedFile::initialize(dfile.get(), flid);
+
+    return std::make_unique<IndexPagedFile>(std::move(dfile), true);
+}
+
+
+PageId IndexPagedFile::allocate_page()
+{
+    return allocate_page_bulk(1);
+}
+
+
+PageId IndexPagedFile::allocate_page_bulk(PageNum new_page_count)
+{
+    PageNum new_first = this->get_last_pid().page_number + 1;
+    size_t alloc_size = new_page_count * parm::PAGE_SIZE;
+
+
+    if (this->dfile->allocate(alloc_size)) {
+        this->header_data.paged_header.page_count += new_page_count;
+        return this->pnum_to_pid(new_first);
+    }
+
+    return INVALID_PID;
+}
+
+
+int IndexPagedFile::free_page(PageId pid)
+{
+    return 0;
+}
+
+
+bool IndexPagedFile::supports_free()
+{
+    return false;
+}
+
+
+int IndexPagedFile::free_page(PageNum pnum)
+{
+    return 0;
+}
+
+
+PageId IndexPagedFile::pnum_to_pid(PageNum pnum)
+{
+    PageId pid;
+    pid.page_number = pnum;
+    pid.file_id = this->header_data.paged_header.flid;
+    return pid;
+}
+
+
+PageNum IndexPagedFile::get_page_count()
+{
+    return this->header_data.paged_header.page_count;
+}
+
+
+PageId IndexPagedFile::get_first_pid()
+{
+    if (this->header_data.paged_header.page_count == 0) {
+        return INVALID_PID;
+    } 
+
+    return this->pnum_to_pid(1);
+}
+
+
+PageId IndexPagedFile::get_last_pid()
+{
+    return this->pnum_to_pid(this->header_data.paged_header.page_count);
+}
+
+
+std::unique_ptr<iter::GenericIterator<Page *>> IndexPagedFile::start_scan(PageId pid)
+{
+    return start_scan(pid.page_number);
+}
+
+
+std::unique_ptr<iter::GenericIterator<Page *>> IndexPagedFile::start_scan(PageNum pnum)
+{
+    return nullptr;
+}
+
+
+int IndexPagedFile::close_file()
+{
+    this->flush_metadata();
+    return this->dfile->close_file();
+}
+
+
+int IndexPagedFile::reopen_file()
+{
+    return this->dfile->reopen();
+}
+
+
+IndexPagedFile::~IndexPagedFile()
+{
+    this->flush_metadata();
+    this->dfile->close_file();
+
+    if (this->is_temporary()) {
+        this->dfile->remove();
+    }
+}
+
+
+int IndexPagedFile::initialize(DirectFile *dfile, FileId flid)
+{
+    if (dfile->allocate(parm::PAGE_SIZE)) {
+        #ifdef NO_BUFFER_MANAGER
+        std::unique_ptr<byte> page = std::unique_ptr<byte>((byte *) std::aligned_alloc(parm::SECTOR_SIZE, parm::PAGE_SIZE));
+
+        IndexPagedFileHeaderData *header = (IndexPagedFileHeaderData *) page.get();
+        #endif
+
+        PagedFile::initialize_pagedfile(page.get(), flid);
+
+        auto offset = PagedFile::pnum_to_offset(IndexPagedFile::header_page_pnum);
+        if (dfile->write(page.get(), parm::PAGE_SIZE, offset)) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    return 0;
+}
+
+
+IndexPagedFile::IndexPagedFile(std::unique_ptr<DirectFile> dfile, bool is_temp_file)
+: PagedFile(std::move(dfile), is_temp_file)
+{
+    #ifdef NO_BUFFER_MANAGER
+    this->buffer = std::unique_ptr<byte>((byte *) aligned_alloc(parm::PAGE_SIZE, parm::PAGE_SIZE));
+    auto offset = PagedFile::pnum_to_offset(IndexPagedFile::header_page_pnum);
+    this->dfile->read(this->buffer.get(), parm::PAGE_SIZE, offset);
+    memcpy(&this->header_data, buffer.get(), sizeof(header_data));
+    #endif
+}
+
+
+#ifdef NO_BUFFER_MANAGER
+void IndexPagedFile::flush_buffer(PageNum pnum)
+{
+    off_t offset = PagedFile::pnum_to_offset(pnum);
+    this->dfile->write(this->buffer.get(), parm::PAGE_SIZE, offset);
+}
+#endif
+
+
+void IndexPagedFile::flush_metadata()
+{
+    #ifdef NO_BUFFER_MANAGER
+    memcpy(buffer.get(), &this->header_data, IndexPagedFileHeaderSize);
+    this->flush_buffer(IndexPagedFile::header_page_pnum);
+    #endif
+}
+
+}
+}
