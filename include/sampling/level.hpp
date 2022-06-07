dcd15ced5a052e1943b5a7f2e58f7362d1b9af68
Douglas Rumbaugh <dbr4@psu.edu>
2022-06-07 17:53:19 -0400

    Sampling: Level initial commit
    Added the initial implementation of BTreeLevel, for LSM sampling

---
+++ include/sampling/level.hpp
@@ -0,0 +1,168 @@
+/*
+ *
+ */
+
+#ifndef H_LEVEL
+#define H_LEVEL
+
+#include "util/base.hpp"
+#include "util/types.hpp"
+#include "io/record.hpp"
+#include "util/global.hpp"
+
+#include "ds/staticbtree.hpp"
+
+namespace lsm { namespace sample {
+
+class BTreeLevel {
+public:
+    /*
+     * Create a new BTreeLevel object of specified run capacity (1=LEVELING,
+     * >1=TIERING) and record capacity. The files vector contains the filenames
+     * for pre-existing BTrees that make up this level. The length of this
+     * vector must be no greater than the run capacity. An empty vector can be
+     * provided if the run is to be created empty, with no pre-existing data.
+     */
+    BTreeLevel(size_t run_capacity, size_t record_capacity,
+               std::vector<io::IndexPagedFile *> files, global::g_state *state,
+               double max_deletion_proportion);
+
+    ~BTreeLevel() = default;
+
+    /*
+     * Return a raw pointer to the specified run within this level,
+     * or nullptr if the specified index is out of range. There
+     * aren't many situations where you should need to access a
+     * run within an LSMTree directly. The returned pointer should
+     * not be freed, and the owner of this pointer does NOT control
+     * the lifetime of the object.
+     */
+    ds::StaticBTree *get_run(size_t idx);
+
+    /*
+     * Place the specified pointer to a run into the level,
+     * and return the index at which it was stored. If there is
+     * no room, or the run cannot be placed for some other reason,
+     * returns -1. Note that this uses std::move to place the
+     * pointer into the Level, and so the passed in pointer will
+     * be nulled.
+     */
+    int emplace_run(std::unique_ptr<ds::StaticBTree> run);
+
+    /*
+     * Returns true if the level's run count is less than its capacity
+     * (i.e. it is a valid target for merging the previous level into),
+     * and false if not.
+     */
+    bool can_emplace_run();
+
+    int merge_with(BTreeLevel *level);
+    int merge_with(std::unique_ptr<ds::StaticBTree> run);
+
+    /*
+     * Determine if a run containing incoming_record_count records can be
+     * merged into this one without exceeding its record capacity. If so,
+     * return true, otherwise return false.
+     */
+    bool can_merge_with(size_t incoming_record_count);
+
+
+    /*
+     * Determine if a level containing incoming_record_count can be merged into
+     * this one without exceeding its record capacity. If so, return true,
+     * otherwise return false.
+     */
+    bool can_merge_with(BTreeLevel *level);
+
+    /*
+     * Merge all of the runs stored on this level into one
+     * new run, and return a pointer to it. If only 1 run exists
+     * on the level, return a pointer to it. If only 1 run exists
+     * and it is not sorted, sort it, and return a pointer to the
+     * new sorted run.
+     *
+     * Leaves the run objects contained within the level untouched
+     */
+    std::unique_ptr<ds::StaticBTree> merge_runs();
+
+    //std::vector<std::unique_ptr<SampleRange>> sample_ranges(size_t start, size_t stop, gsl_rng *rng);
+
+    /*
+     * Remove the references to all runs within the level, and
+     * reset the run count and record count to 0.
+     */
+    void truncate();
+
+    /*
+     * Perform a search for the specified key within this level, and return a
+     * Record object representing the first record in which a match is found having
+     * a timestamp less than or equal to the specified one, or nullptr if no
+     * match is found. 
+     */
+     Record get_by_key(byte *key, Timestamp time=0);
+
+    /*
+     * Perform a search for the specified key within this level and mark the
+     * first corresponding record found with a matching value and a timestamp less
+     * than or equal to the specified one as deleted. If a record is marked
+     * deleted, returns 1. Otherwise, returns 0.
+     */
+    int remove(byte *key, std::byte *value, Timestamp time=0);
+
+    /*
+     * Returns the maximum number of records that this level
+     * can store.
+     */
+    size_t get_record_capacity();
+
+    /*
+     * Returns the maximum number of runs that can be present
+     * on this level.
+     */
+    size_t get_run_capacity();
+
+    /*
+     * Returns the current number of records that are stored
+     * on this level.
+     */
+    size_t get_record_count();
+
+    /*
+     * Returns the current number of runs that are stored on
+     * this level.
+     */
+    size_t get_run_count();
+
+    /*
+     * Returns the current amount of memory used by all of
+     * the runs within this level.
+     */
+    size_t get_memory_utilization();
+
+    /*
+     * Print a graphical representation of this level to stdout, 
+     * showing each run on the level, with its record count and
+     * capacity.
+     */
+    void print_level();
+
+private:
+    size_t record_capacity; // the maximum number of records on this level
+    size_t record_count;    // the current number of records on this level
+
+    size_t run_capacity;    // the maximum number of runs on this level
+    size_t run_count;       // the current_number of runs on this level
+
+    double max_deleted_prop; // The largest allowed proportion of deleted
+                             // records before performing a compaction
+    
+    global::g_state *state;  // tracks global state inherited from the parent
+                             // environment (probably an LSMTree object)
+    catalog::RecordCmpFunc record_cmp;
+    catalog::KeyCmpFunc key_cmp;
+
+    std::vector<std::unique_ptr<ds::StaticBTree>> runs; // the runs stored on this level
+    };
+}}
+#endif
+
---
+++ src/sampling/level.cpp
@@ -0,0 +1,210 @@
+/*
+ *
+ */
+
+#include "sampling/level.hpp"
+
+namespace lsm { namespace sample {
+
+BTreeLevel::BTreeLevel(size_t run_capacity, size_t record_capacity, 
+                       std::vector<io::IndexPagedFile *> files, 
+                       global::g_state *state,
+                       double max_deletion_proportion)
+{
+    this->run_capacity = run_capacity;
+    this->record_capacity = record_capacity;
+    this->max_deleted_prop = max_deletion_proportion;
+
+    this->run_count = 0;
+    this->record_count = 0;
+
+    this->runs = std::vector<std::unique_ptr<ds::StaticBTree>>(run_capacity);
+    this->state = state;
+
+    this->record_cmp = state->record_schema->get_record_cmp();
+    this->key_cmp = state->record_schema->get_key_cmp();
+
+    for (size_t i=0; i<files.size(); i++) {
+        if (i < this->run_capacity) {
+            this->runs[i] = std::make_unique<ds::StaticBTree>(files[i], state);
+            this->run_count++;
+            this->record_count += this->runs[i]->get_record_count();
+        }
+    }
+}
+
+
+ds::StaticBTree *BTreeLevel::get_run(size_t idx) 
+{
+    if (idx <= this->run_capacity) {
+        return nullptr;
+    }
+
+    return this->runs[idx].get();
+}
+
+
+int BTreeLevel::emplace_run(std::unique_ptr<ds::StaticBTree> run)
+{
+    if (this->run_count == this->run_capacity) {
+        return 0; // no room
+    }
+
+    for (size_t i=0; i<this->run_capacity; i++) {
+        if (this->runs[i].get() == nullptr) {
+            this->runs[i] = std::move(run);
+            this->run_count++;
+            this->record_count += this->runs[i]->get_record_count();
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+
+bool BTreeLevel::can_emplace_run() 
+{
+    return this->run_count < this->run_capacity;
+}
+
+
+bool BTreeLevel::can_merge_with(size_t incoming_record_count)
+{
+    if (this->run_count < this->run_capacity) {
+        return true;
+    } else if (this->run_capacity == 1 && this->record_count + incoming_record_count <= this->record_capacity) {
+        return true;
+    }
+
+    return false;
+}
+
+
+bool BTreeLevel::can_merge_with(BTreeLevel *level) 
+{
+    return this->can_merge_with(level->get_record_count());
+}
+
+
+io::Record BTreeLevel::get_by_key(byte *key, Timestamp time) 
+{
+    return io::Record();
+}
+
+
+int BTreeLevel::remove(byte *key, std::byte *value, Timestamp time)
+{
+    return 0;
+}
+
+
+void BTreeLevel::truncate()
+{
+    // Because files are std::moved from level to level, this should be safe to
+    // do. Any file that has been relocated will no longer be here, so there
+    // isn't any risk of closing a file that some other level is currently
+    // using. This will, of course, need to be revisited for concurrency.
+    for (size_t i=0; i<this->run_capacity; i++) {
+        if (this->runs[i]) {
+            auto pfile = this->runs[i]->get_pfile();
+            this->runs[i] = nullptr;
+            this->state->file_manager->close_file(pfile);
+        }
+    }
+
+    this->run_count = 0;
+    this->record_count = 0;
+}
+
+
+std::unique_ptr<ds::StaticBTree> BTreeLevel::merge_runs()
+{
+    std::vector<std::unique_ptr<iter::GenericIterator<io::Record>>> iters;
+    PageNum page_cnt = 0;
+    for (size_t i=0; i<this->run_capacity; i++) {
+        if (this->runs[i]) {
+            iters.push_back(this->runs[i]->start_scan());
+            page_cnt += this->runs[i]->get_leaf_page_count();
+        }
+    }
+
+    auto merge = std::make_unique<iter::MergeIterator>(iters, this->record_cmp);
+
+    return ds::StaticBTree::create(std::move(merge), page_cnt, this->state);
+}
+
+
+int BTreeLevel::merge_with(std::unique_ptr<ds::StaticBTree> new_run)
+{
+    // For tiering, we emplace the run if we can. This also catches the 
+    // case of merging into an empty level under leveling.
+    if (this->run_count < this->run_capacity) {
+        this->emplace_run(std::move(new_run));
+        return 1;
+    }
+
+    // For leveling, we merge the new run with the run on this level (there
+    // will only ever be one)
+    if (this->can_merge_with(new_run->get_record_count())) {
+        std::vector<std::unique_ptr<iter::GenericIterator<io::Record>>> iters(2);
+        iters[0] = this->runs[0]->start_scan();
+        iters[1] = new_run->start_scan();
+        
+        auto merge_itr = std::make_unique<iter::MergeIterator>(iters, this->record_cmp);
+        PageNum page_cnt = new_run->get_leaf_page_count() + this->runs[0]->get_leaf_page_count();
+
+        auto new_btree = ds::StaticBTree::create(std::move(merge_itr), page_cnt, this->state);
+
+        // abort if the creation of the new, merged, level failed for some reason.
+        if (!new_btree) {
+            return 0;
+        }
+
+        // FIXME: will need a different approach when concurrency comes into place
+        this->truncate();
+        this->emplace_run(std::move(new_btree));
+        return 1;
+    }
+
+    // This level could not support the merge due to being at run and/or record capacity.
+    return 0;
+}
+
+
+size_t BTreeLevel::get_record_capacity()
+{
+    return this->record_capacity;
+}
+
+
+size_t BTreeLevel::get_record_count()
+{
+    return this->record_count;
+}
+
+
+size_t BTreeLevel::get_run_capacity()
+{
+    return this->run_capacity;
+}
+
+
+size_t BTreeLevel::get_run_count()
+{
+    return this->run_count;
+}
+
+
+size_t BTreeLevel::get_memory_utilization()
+{
+    return 0;
+}
+
+
+void BTreeLevel::print_level()
+{
+    return;
+}
+
+}}
