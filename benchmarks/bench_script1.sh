885df926a15b22ca5ab9ed3bb5efe023c8b58db5
Douglas Rumbaugh <dbr4@psu.edu>
2022-07-06 14:35:51 -0400

    Updated benchmark results

--- benchmarks/bench_script1.sh
+++ benchmarks/bench_script1.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-memtable=( 10000 50000 100000 150000 )
-data_size=( 1000000 50000000 10000000 )
+memtable=( 100000 )
+data_size=( 1000000 10000000 )
 scale_factor=( 2 3 4 5 )
-trials=2
+trials=3
 
 for table in ${memtable[@]}; do
     for data in ${data_size[@]}; do
         for sf in ${scale_factor[@]}; do
             fname="benchmarks/results/${table}_${data}_${sf}.dat"
-            echo "rejections attempts memtable_time boundary_time walker_time sample_time" > $fname
+            echo "record_count tree_height sample_size insert_time rejections attempts cache_misses buffer_proc_time bounds_time walker_time sample_time rej_check_time" > $fname
 
             echo "Running trials for $data $table $sf"
             for i in $(seq 1 $trials); do
-                benchmarks/first_bench "$data" "$table" "$sf" >> "$fname"
+                benchmarks/first_bench "$data" "$table" "$sf" 0 0 >> "$fname"
                 rm benchmarks/data/default_bench/*
             done
             echo "Trials done"
