d25ea479db7fe4b1b99671ef495d5b466b698b4b
Douglas Rumbaugh <doug@douglasrumbaugh.com>
2022-07-13 15:49:08 -0400

    insert_bench: Assorted revisions + fixes

--- benchmarks/insert_bench.cpp
+++ benchmarks/insert_bench.cpp
@@ -11,49 +11,18 @@
 #include <fstream>
 #include <sstream>
 #include <unordered_set>
-
-static void load_data(std::fstream *file, lsm::sampling::LSMTree *lsmtree, size_t count) 
-{
-    std::string line;
-    size_t i=0;
-    while (i < count && getline(*file, line, '\n')) {
-        std::stringstream line_stream(line);
-        std::string key_field;
-        std::string value_field;
-
-        std::getline(line_stream, value_field, ' ');
-        std::getline(line_stream, key_field, ' ');
-
-        std::byte *key = (std::byte *) key_field.c_str();
-        std::byte *val = (std::byte *) value_field.c_str();
-
-        lsmtree->insert(key, val);
-        i++;
-    }
-}
-
-
-static std::pair<int64_t, int64_t> sample_range(int64_t min, int64_t max, double selectivity, lsm::global::g_state *state)
-{
-    size_t range_length = max - min * selectivity;
-
-    int64_t max_bottom = max - range_length;
-    int64_t bottom = gsl_rng_uniform_int(state->rng, max_bottom);
-
-    return std::pair<int64_t, int64_t> {bottom, bottom + range_length};
-}
-
+#include <string>
 
 static bool next_record(std::fstream *file, std::byte *key, std::byte *val)
 {
     std::string line;
-    if (getline(*file, line, '\n')) {
+    if (std::getline(*file, line, '\n')) {
         std::stringstream line_stream(line);
         std::string key_field;
         std::string value_field;
 
-        std::getline(line_stream, value_field, ' ');
-        std::getline(line_stream, key_field, ' ');
+        std::getline(line_stream, value_field, '\t');
+        std::getline(line_stream, key_field, '\t');
 
         *((int64_t*) key) = atol(key_field.c_str());
         *((int64_t*) val) = atol(value_field.c_str());
@@ -63,6 +32,34 @@ static bool next_record(std::fstream *file, std::byte *key, std::byte *val)
     key = nullptr;
     val = nullptr;
     return false;
+}
+
+
+static void load_data(std::fstream *file, lsm::sampling::LSMTree *lsmtree, size_t count, size_t key_len, size_t val_len) 
+{
+    std::string line;
+
+    auto key_buf = std::make_unique<std::byte[]>(key_len);
+    auto val_buf = std::make_unique<std::byte[]>(val_len);
+    
+    for (size_t i=0; i<count; i++) {
+        if (!next_record(file, key_buf.get(), val_buf.get())) {
+            break;
+        }
+
+        lsmtree->insert(key_buf.get(), val_buf.get());
+    }
+}
+
+
+static std::pair<int64_t, int64_t> sample_range(int64_t min, int64_t max, double selectivity, lsm::global::g_state *state)
+{
+    size_t range_length = (max - min) * selectivity;
+
+    int64_t max_bottom = max - range_length;
+    int64_t bottom = gsl_rng_uniform_int(state->rng, max_bottom);
+
+    return std::pair<int64_t, int64_t> {bottom, bottom + range_length};
 }
 
 
@@ -104,8 +101,10 @@ static bool benchmark(lsm::sampling::LSMTree *tree, std::fstream *file,
     size_t attempts = 0;
     size_t cache_misses = 0;
 
-    for (size_t i=0; i<samples; i++) {
+    size_t total_rejections = 0;
+    size_t total_attempts = 0;
 
+    for (size_t i=0; i<samples; i++) {
         auto range = sample_range(min_key, max_key, selectivity, tree->global_state());
 
         tree->range_sample_bench((std::byte *) &range.first,
@@ -115,10 +114,19 @@ static bool benchmark(lsm::sampling::LSMTree *tree, std::fstream *file,
                                  &walker_time, &sample_time, &reject_time);
 
         io_time += tree->cache()->io_time();
+        sample_time -= tree->cache()->io_time();
         cache_misses += tree->cache()->cache_misses();
+
+        total_rejections += rejections;
+        total_attempts += attempts;
     }
 
-    fprintf(stdout, "%ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld\n", tree->record_count(), tree->depth(), sample_size, per_insert, rejections / samples, attempts / samples, cache_misses / samples, buffer_time / samples, bounds_time / samples, walker_time / samples, sample_time / samples, io_time / samples, reject_time / samples);
+    fprintf(stdout, "%ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld\n",
+            tree->record_count(), tree->depth(), sample_size, per_insert,
+            total_rejections / samples, total_attempts / samples, cache_misses / samples,
+            buffer_time / samples, bounds_time / samples, walker_time /
+            samples, sample_time / samples, io_time / samples, reject_time /
+            samples);
 
     return !out_of_data;
 }
@@ -126,6 +134,7 @@ static bool benchmark(lsm::sampling::LSMTree *tree, std::fstream *file,
 
 int main(int argc, char **argv)
 {
+    size_t record_count = 10000000;
     if (argc < 5) {
         fprintf(stderr, "Usage: insert_bench <filename> <memtable_size> <scale_factor> <selectivity>\n");
         exit(EXIT_FAILURE);
@@ -142,7 +151,7 @@ int main(int argc, char **argv)
 
     // use for selectivity calculations
     int64_t min_key = 0;
-    int64_t max_key = 10000000000;
+    int64_t max_key = record_count - 1;
 
     auto state = lsm::bench::bench_state(key_size, val_size);
 
@@ -162,12 +171,12 @@ int main(int argc, char **argv)
 
     // warm up the tree with initial_insertions number of initially inserted
     // records
-    size_t initial_insertions = 1000000000;
-    load_data(&datafile, sampling_lsm.get(), initial_insertions);
+    size_t initial_insertions = .1 * record_count;
+    load_data(&datafile, sampling_lsm.get(), initial_insertions, key_size, val_size);
 
     size_t sample_size = 1000;
-    size_t samples = 10000;
-    size_t inserts = 500000000;
+    size_t samples = 1000;
+    size_t inserts = .1 * record_count;
 
     while (benchmark(sampling_lsm.get(), &datafile, inserts, samples,
                      sample_size, min_key, max_key, selectivity)) {
