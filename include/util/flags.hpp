9aff618e457d8dbe5354afa0382e6961b2d69ea6
Douglas Rumbaugh <doug@douglasrumbaugh.com>
2022-08-18 15:13:27 -0400

    flags.hpp: missed committing this
    Should've gone in with the LSM Tree flag changes.

---
+++ include/util/flags.hpp
@@ -0,0 +1,25 @@
+/*
+ *
+ */
+
+#ifndef H_FLAGS
+#define H_FLAGS
+
+#include "util/types.hpp"
+
+typedef uint64_t flag;
+
+constexpr flag F_NONE = 0;
+constexpr flag F_FLAG0 = 1;
+constexpr flag F_FLAG1 = 2;
+constexpr flag F_FLAG2 = 4;
+constexpr flag F_FLAG3 = 8;
+constexpr flag F_FLAG4 = 16;
+constexpr flag F_FLAG5 = 32;
+constexpr flag F_FLAG6 = 64;
+constexpr flag F_FLAG7 = 128;
+constexpr flag F_FLAG8 = 256;
+constexpr flag F_FLAG9 = 512;
+constexpr flag F_FLAG10 = 1024;
+
+#endif
