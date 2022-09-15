743bd37d916ac242666dde66a628729a47a93b40
Douglas Rumbaugh <doug@douglasrumbaugh.com>
2022-09-14 21:38:02 -0400

    Missed header from last commit

---
+++ tests/testing.h
@@ -0,0 +1,47 @@
+/*
+ *
+ *
+ */
+
+#include <string>
+
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "util/types.h"
+#include "util/base.h"
+
+bool initialize_test_file(std::string fname, size_t page_cnt)
+{
+    auto flags = O_RDWR | O_CREAT | O_TRUNC;
+    mode_t mode = 0640;
+    char *page = nullptr;
+
+    int fd = open(fname.c_str(), flags, mode);
+    if (fd == -1) {
+        goto error;
+    }
+
+    page = (char *) aligned_alloc(lsm::SECTOR_SIZE, lsm::PAGE_SIZE);
+    if (!page) {
+        goto error_opened;
+    }
+
+    for (size_t i=0; i<=page_cnt; i++) {
+        *((int *) page) = i;
+        if (write(fd, page, lsm::PAGE_SIZE) == -1) {
+            goto error_alloced;
+        }
+    }
+
+    return 1;
+
+error_alloced:
+    free(page);
+
+error_opened:
+    close(fd);
+
+error:
+    return 0;
+}
