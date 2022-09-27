4df9340192b934ad8e1e480ad3bf3d677a0c4dd2
Dong Xie <dongx@psu.edu>
2022-09-26 22:24:16 -0400

    update.

--- include/lsm/InMemRun.h
+++ include/lsm/InMemRun.h
@@ -13,13 +13,70 @@ public:
     // InMemISAM Tree Node: |8 keys|8 pointers|
     // Each node: 128 bytes;
 
+    constexpr size_t inmem_isam_fanout = 8;
+    constexpr size_t inmem_isam_node_size = 128;
+
     struct MergeCursor {
         char* ptr;
         char* target;
     };
 
+    InMemRun(MemTable* mem_table) {
+        m_data = (char*)std::aligned_alloc(SECTOR_SIZE, mem_table->get_record_count() * record_size);
+        size_t offset = 0;
+        m_reccnt = 0;
+
+        while (base < stop) {
+            if (is_tombstone(base) || ((base + record_size < stop) && !record_cmp(base+ record_size, base) && is_tombstone(base + record_size))) {
+                base += record_size * 2;
+            } else {
+                memcpy(m_data + offset, base, record_size);
+                offset += record_size;
+                ++m_reccnt;
+                base += record_size;
+            }
+        }
+
+        size_t leaf_level_nodes = m_reccnt / inmem_isam_fanout + (m_reccnt % inmem_isam_fanout != 0);
+        size_t level_nodes = leaf_level_nodes;
+        size_t all_nodes = level_nodes;
+        while (level_nodes > 1) {
+            level_nodes = level_nodes / inmem_isam_fanout;
+            all_nodes += level_nodes;
+        }
+
+        m_isam_nodes = (char*)std::aligned_alloc(SECTOR_SIZE, all_nodes * inmem_isam_node_size);
+        const char* current_node = m_isam_nodes;
+        const char* leaf_base = m_data;
+        const char* leaf_stop = m_data + m_reccnt * record_size;
+        size_t key_skip = key_size * inmem_isam_fanout;
+        while (leaf_base < leaf_stop) {
+            for (size_t i = 0; i < inmem_isam_fanout; ++i) {
+                auto rec_ptr = leaf_base + i * record_size;
+                if (rec_ptr >= leaf_stop) break;
+                memcpy(current_node + key_size * i, get_key(), key_size);
+                memcpy(current_node + key_skip + sizeof(const char*) * i, &rec_ptr, sizeof(const char*));
+            }
+            node_offset += inmem_isam_node_size;
+            leaf_base += inmem_isam_fanout * record_size;
+        }
+
+        auto level_start = m_isam_nodes;
+        auto level_stop = current_node;
+        auto current_level_node_cnt = (level_stop - level_start) / inmem_isam_node_size;
+        assert(leaf_level_nodes == current_level_node_cnt);
+        while (current_level_node_cnt > 1) {
+            for (size_t i = 0; i < inmem_isam_fanout; ++i) {
+                auto node_ptr = level_start + i * 
+            }
+        }
+        
+
+
+    }
+
     // Master interface to create an ondisk Run.
-    InMemRun(MemTable* mem_table, std::vector<InMemRun*> runs) {
+    InMemRun(std::vector<InMemRun*> runs) {
         std::vector<MergeCursor> cursors;
         cursors.reserve(runs.size() + 1);
 
