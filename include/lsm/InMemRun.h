9eeab4d67f1206eef07e160fce03b964f60ef3a6
Dong Xie <dongx@psu.edu>
2022-09-28 17:13:14 -0400

    update.

--- include/lsm/InMemRun.h
+++ include/lsm/InMemRun.h
@@ -11,6 +11,7 @@ namespace lsm {
 
 constexpr size_t inmem_isam_fanout = 8;
 constexpr size_t inmem_isam_node_size = 128;
+constexpr size_t inmem_isam_nope_keyskip = key_size * inmem_isam_fanout;
 
 class InMemRun {
 public:
@@ -28,6 +29,7 @@ public:
 
     InMemRun(MemTable* mem_table) {
         m_data = (char*)std::aligned_alloc(SECTOR_SIZE, mem_table->get_record_count() * record_size);
+        //memset(m_data, 0, mem_table->get_record_count() * record_size);
         size_t offset = 0;
         m_reccnt = 0;
         auto base = mem_table->sorted_output();
@@ -65,6 +67,7 @@ public:
         }
 
         m_data = (char*)std::aligned_alloc(SECTOR_SIZE, attemp_reccnt * record_size);
+        //memset(m_data, 0, mem_table->get_record_count() * record_size);
         size_t offset = 0;
         
         while (pq.size()) {
@@ -113,8 +116,29 @@ public:
         return m_data + idx * record_size;
     }
 
-    size_t get_lower_bound(const char* key) const{
-        
+    size_t get_lower_bound(const char* key) const {
+        //| size | keys * 7 | ptrs * 8 |
+
+        char* now = m_root;
+        while (!is_leaf(now)) {
+            char** child_ptr = (char**)(now + inmem_isam_nope_keyskip);
+            uint8_t ptr_offset = 0;
+            const char* sep_key = now + sizeof(double);
+            char* next = nullptr;
+            while (ptr_offset < 7) {
+                if (nullptr == *(child_ptr + sizeof(char*)) || key_cmp(key, sep_key) == -1) {
+                    next = *child_ptr;
+                    break;
+                } else {
+                    sep_key += key_size;
+                    child_ptr += sizeof(char*);
+                    ++ptr_offset;
+                }
+            }
+            now = next ? next : *child_ptr;
+        }
+
+        return (now - m_data) / record_size;
     }
 
     size_t get_upper_bound(const char* key) const {
@@ -125,18 +149,19 @@ public:
 private:
     void build_internal_levels() {
         size_t leaf_level_nodes = m_reccnt / inmem_isam_fanout + (m_reccnt % inmem_isam_fanout != 0);
-        size_t level_nodes = leaf_level_nodes;
-        size_t all_nodes = level_nodes;
-        while (level_nodes > 1) {
-            level_nodes = level_nodes / inmem_isam_fanout;
-            all_nodes += level_nodes;
+        size_t level_node_cnt = leaf_level_nodes;
+        size_t node_cnt = level_node_cnt;
+        while (level_node_cnt > 1) {
+            level_node_cnt = level_node_cnt / inmem_isam_fanout;
+            node_cnt += level_node_cnt;
         }
 
-        m_isam_nodes = (char*)std::aligned_alloc(SECTOR_SIZE, all_nodes * inmem_isam_node_size);
+        m_isam_nodes = (char*)std::aligned_alloc(SECTOR_SIZE, node_cnt * inmem_isam_node_size);
+        memset(m_isam_nodes, 0, node_cnt * inmem_isam_node_size);
+
         char* current_node = m_isam_nodes;
         const char* leaf_base = m_data;
         const char* leaf_stop = m_data + m_reccnt * record_size;
-        size_t key_skip = key_size * inmem_isam_fanout;
         while (leaf_base < leaf_stop) {
             double node_weight = 0.0;
             for (size_t i = 0; i < inmem_isam_fanout; ++i) {
@@ -145,7 +170,7 @@ private:
                 node_weight += 1.0;
                 if (i == 0) continue;
                 memcpy(current_node + key_size * i, get_key(rec_ptr), key_size);
-                memcpy(current_node + key_skip + sizeof(char*) * i, &rec_ptr, sizeof(char*));
+                memcpy(current_node + inmem_isam_nope_keyskip + sizeof(char*) * i, &rec_ptr, sizeof(char*));
             }
             memcpy(current_node, &node_weight, sizeof(double));
             current_node += inmem_isam_node_size;
@@ -168,7 +193,7 @@ private:
                     node_weight += child_weight;
                     if (i == 0) continue;
                     memcpy(current_node + key_size * i, node_ptr, key_size);
-                    memcpy(current_node + key_skip + sizeof(char *), &node_ptr, sizeof(char*));
+                    memcpy(current_node + inmem_isam_nope_keyskip + sizeof(char *), &node_ptr, sizeof(char*));
                 }
                 now += inmem_isam_fanout * inmem_isam_node_size;
                 current_node += inmem_isam_node_size;
@@ -181,6 +206,10 @@ private:
         assert(current_level_node_cnt == 1);
 
         m_root = level_start;
+    }
+
+    bool is_leaf(const char* ptr) const {
+        return ptr >= m_data && ptr < m_data + m_reccnt * record_size;
     }
 
     // Members: sorted data, internal ISAM levels, reccnt;
