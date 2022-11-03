c21a859b0fd7dd8338950e36a81fdf0fd917520b
Douglas Rumbaugh <dbr4@psu.edu>
2022-11-03 12:03:19 -0400

    Data processing scripts for benchmarking

---
+++ bin/utilities/data_proc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]]; then
+    echo 'gen_data <count> <filename>'
+    exit 1
+fi
+
+count=$1
+fname=$2
+
+bin/utilities/data_generator "$count" "${fname}_raw"
+
+# larger than memory shuffle via decorate-sort-undecorate with awk, followed by
+# line numbering (for simulated external pkey) and cleanup
+awk 'BEGIN{srand();} {printf "%0.15f\t%s\n", rand(), $0;}' "${fname}_raw" | sort -n | cut  -f 2- | cat -n | sed 's/^ *//g' > "$fname"
+
+rm "${fname}_raw"
---
+++ src/utilities/data_generator.cpp
@@ -0,0 +1,31 @@
+/*
+ *
+ *
+ */
+
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char **argv) 
+{
+    if (argc < 2) {
+        fprintf(stderr, "data_generator <count> [filename]\n");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *output_file;
+    if (argc >= 3) {
+        auto fname = argv[2];
+        output_file = fopen(fname, "w");
+    } else {
+        output_file = stdout;
+    }
+
+    size_t count = atol(argv[1]);
+
+    for (size_t i=0; i<count; i++) {
+        fprintf(output_file, "%ld\n", i);
+    }
+
+    exit(EXIT_SUCCESS);
+}
