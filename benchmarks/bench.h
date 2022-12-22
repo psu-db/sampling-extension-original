e54e70a9da6232796787fe0283b77f20cd29b136
Douglas B. Rumbaugh <doug@douglasrumbaugh.com>
2022-12-22 11:32:19 -0500

    Removed deletes from warmup

--- benchmarks/bench.h
+++ benchmarks/bench.h
@@ -1,6 +1,7 @@
 #ifndef H_BENCH
 #define H_BENCH
 #include "lsm/LsmTree.h"
+#include "ds/BTree.h"
 
 #include <cstdlib>
 #include <cstdio>
@@ -20,6 +21,14 @@ typedef std::pair<lsm::key_type, lsm::key_type> key_range;
 typedef std::pair<char*, char*> record;
 typedef std::pair<std::shared_ptr<char[]>, std::shared_ptr<char[]>> shared_record;
 
+typedef std::pair<lsm::key_type, lsm::value_type> btree_record;
+struct btree_key_extract {
+    static const lsm::key_type &get(const btree_record &v) {
+        return v.first;
+    }
+};
+typedef tlx::BTree<lsm::key_type, btree_record, btree_key_extract> TreeMap;
+
 static gsl_rng *g_rng;
 static std::set<shared_record> *g_to_delete;
 
@@ -36,6 +45,7 @@ static unsigned int get_random_seed()
 
     return seed;
 }
+
 
 static void init_bench_rng(unsigned int seed, const gsl_rng_type *type)
 {
@@ -59,6 +69,7 @@ static void delete_bench_env()
     delete g_to_delete;
 }
 
+
 static record create_record()
 {
     auto key_buf = new char[lsm::key_size];
@@ -66,6 +77,8 @@ static record create_record()
 
     return {key_buf, val_buf};
 }
+
+
 static shared_record create_shared_record()
 {
     auto key_buf = new char[lsm::key_size];
@@ -75,6 +88,7 @@ static shared_record create_shared_record()
 
     return {key_ptr, val_ptr};
 }
+
 
 static bool next_record(std::fstream *file, char *key, char *val)
 {
@@ -119,15 +133,6 @@ static void warmup(std::fstream *file, lsm::LSMTree *lsmtree, size_t count, doub
             memcpy(del_val_buf, val_buf.get(), lsm::value_size);
             g_to_delete->insert({std::shared_ptr<char[]>(del_key_buf), std::shared_ptr<char[]>(del_val_buf)});
         }
-
-        if (gsl_rng_uniform(g_rng) < delete_prop) {
-            std::vector<std::pair<std::shared_ptr<char[]>, std::shared_ptr<char[]>>> del_vec;
-            std::sample(g_to_delete->begin(), g_to_delete->end(), std::back_inserter(del_vec), 1, std::mt19937{std::random_device{}()});
-            if (del_vec.size() > 0) {
-                lsmtree->append(del_vec[0].first.get(), del_vec[0].second.get(), true, g_rng);
-                g_to_delete->erase(del_vec[0]);
-            }
-        }
     }
 }
 
@@ -161,5 +166,34 @@ static void reset_lsm_perf_metrics() {
 }
 
 
+static void build_lsm_tree(lsm::LSMTree *tree, std::fstream *file) {
+    auto key_buf = new char[lsm::key_size]();
+    auto val_buf = new char[lsm::value_size]();
+
+    size_t i=0;
+    while (next_record(file, key_buf, val_buf)) {
+        auto res = tree->append(key_buf, val_buf, false, g_rng);
+        assert(res);
+    }
+    delete[] key_buf;
+    delete[] val_buf;
+}
+
+
+static void build_btree(TreeMap *tree, std::fstream *file) {
+    // for looking at the insert time distribution
+    auto key_buf = new char[lsm::key_size]();
+    auto val_buf = new char[lsm::value_size]();
+
+    size_t i=0;
+    while (next_record(file, key_buf, val_buf)) {
+        lsm::key_type key = *(lsm::key_type*) key_buf;
+        lsm::value_type val = *(lsm::value_type*) val_buf;
+        auto res = tree->insert({key, val});
+        assert(res.second);
+    }
+    delete[] key_buf;
+    delete[] val_buf;
+}
 
 #endif // H_BENCH
