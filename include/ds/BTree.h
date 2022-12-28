bc07e52c9270b6537097ff5869b5d3f7769172d6
Douglas Rumbaugh <dbr4@psu.edu>
2022-12-28 16:01:24 -0500

    Updated btree fanout

--- include/ds/BTree.h
+++ include/ds/BTree.h
@@ -88,13 +88,13 @@ struct btree_default_traits {
 
     //! Number of slots in each leaf of the tree. Estimated so that each node
     //! has a size of about 256 bytes.
-    static const int leaf_slots =
-        TLX_BTREE_MAX(4, 256 / (sizeof(Value)));
+    static const int leaf_slots = 4;
+        //TLX_BTREE_MAX(4, 256 / (sizeof(Value)));
 
     //! Number of slots in each inner node of the tree. Estimated so that each
     //! node has a size of about 256 bytes.
-    static const int inner_slots =
-        TLX_BTREE_MAX(4, 256 / (sizeof(Key) + sizeof(void*)));
+    static const int inner_slots = 4;
+        //TLX_BTREE_MAX(4, 256 / (sizeof(Key) + sizeof(void*)));
 
     //! As of stx-btree-0.9, the code does linear search in find_lower() and
     //! find_upper() instead of binary_search, unless the node size is larger
