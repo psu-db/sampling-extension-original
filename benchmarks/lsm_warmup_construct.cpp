d7091362720021bdb5622258a23ab99bf3e1e7c4
Dong Xie <dongx@psu.edu>
2023-01-11 21:54:11 -0500

    Change default params in warmup.

--- benchmarks/lsm_warmup_construct.cpp
+++ benchmarks/lsm_warmup_construct.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char **argv)
 {
-    if (argc < 8) {
+    if (argc < 5) {
         fprintf(stderr, "Usage: insert_bench <filename> <record_count> <memtable_size> <scale_factor> <memory_levels> <delete_proportion> <max_delete_proportion>\n");
         exit(EXIT_FAILURE);
     }
@@ -12,17 +12,18 @@ int main(int argc, char **argv)
     std::string filename = std::string(argv[1]);
     size_t record_count = atol(argv[2]);
     size_t memtable_size = atol(argv[3]);
-    size_t scale_factor = atol(argv[4]);
-    //double selectivity = atof(argv[5]);
-    size_t memory_levels = atol(argv[5]);
-    double delete_prop = atof(argv[6]);
-    double max_delete_prop = atof(argv[7]);
-    double insert_batch = (argc == 9) ? atof(argv[8]) : 0.1;
+    double delete_prop = atof(argv[4]);
+    // size_t scale_factor = atol(argv[4]);
+    // //double selectivity = atof(argv[5]);
+    // size_t memory_levels = atol(argv[5]);
+    
+    // double max_delete_prop = atof(argv[7]);
+    double insert_batch = (argc == 6) ? atof(argv[5]) : 0.8;
 
     std::string root_dir = "benchmarks/data/default_bench";
     init_bench_env(true);
 
-    auto sampling_lsm = lsm::LSMTree(root_dir, memtable_size, memtable_size*3, scale_factor, memory_levels, max_delete_prop, g_rng);
+    auto sampling_tree = lsm::LSMTree(root_dir, 15000, 15000*3, 10, 1000, 0.05, g_rng);
 
     std::fstream datafile;
     datafile.open(filename, std::ios::in);
@@ -30,9 +31,9 @@ int main(int argc, char **argv)
     // warm up the tree with initial_insertions number of initially inserted
     // records
     size_t initial_insertions = insert_batch * record_count;
-    warmup(&datafile, &sampling_lsm, initial_insertions, delete_prop);
+    warmup(&datafile, &sampling_tree, initial_insertions, delete_prop);
 
-    sampling_lsm.persist_tree(g_rng);
+    sampling_tree.persist_tree(g_rng);
 
     delete_bench_env();
     exit(EXIT_SUCCESS);
